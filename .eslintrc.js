9668add79fb8e2c4e417d3729cc87ab253524994
Donghee Kim <kimdhoe1@gmail.com>
2019-01-16 21:58:55 +0900

    control one-segment snake using arrow keys

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,50 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    extends: ["standard"],
+    plugins: ["standard", "react"],
+    rules: {
+        "no-var": "error", // optional, recommended when using es6+
+        "no-unused-vars": 1, // recommended
+        "arrow-spacing": ["error", { before: true, after: true }], // recommended
+        indent: ["error", 2, { "SwitchCase": 1 }],
+        "comma-dangle": [
+            "error",
+            {
+                objects: "only-multiline",
+                arrays: "only-multiline",
+                imports: "never",
+                exports: "never",
+                functions: "never",
+            },
+        ],
+
+        // options to emulate prettier setup
+        semi: ["error", "never"],
+        "max-len": ["error", { code: 80 }],
+        // "template-curly-spacing": ["error", "always"],
+        "arrow-parens": ["error", "as-needed"],
+
+        // standard.js
+        "space-before-function-paren": [
+            "error",
+            {
+                named: "always",
+                anonymous: "always",
+                asyncArrow: "always",
+            },
+        ],
+
+        // standard plugin - options
+        "standard/object-curly-even-spacing": ["error", "either"],
+        "standard/array-bracket-even-spacing": ["error", "either"],
+        "standard/computed-property-even-spacing": ["error", "even"],
+        "standard/no-callback-literal": ["error", ["cb", "callback"]],
+
+        // react plugin - options
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
     },
-    "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaVersion: 8, // optional, recommended 6+
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+}
--- src/components/game/index.js
+++ src/components/game/index.js
@@ -4,7 +4,16 @@ import React from 'react'
 // Constants
 // -----------------------------------------------------------------------------
 
+// SCENE_SIZE :: number
+// Size of the scene. Describes how many segments fit into a row or a column.
+// e.g. 20: Up to twenty segments can fit into one row/column.
 const SCENE_SIZE = 20
+// SEGMENT_SIZE :: number
+// A percentage value that tells how much space a segment occupies in a row/col.
+// That is:
+//   SEGMENT_SIZE * SCENE_SIZE = 100
+// e.g. 5: 5%
+const SEGMENT_SIZE = 100 / SCENE_SIZE
 const SNAKE_COLOR = '#5c940d'
 const FOOD_COLOR = '#e67700'
 
@@ -42,44 +51,175 @@ const directions = {
   left: 'LEFT',
 }
 
+const initialState = {
+  direction: directions.right,
+  snake: snake([ segment(3, 3) ]),
+}
+
 // An Action is one of:
+//   - TickAction
+//   - GoUpAction
+//   - GoRightAction
+//   - GoDownAction
+//   - GoLeftAction
+
+// A TickAction is an object:
+//   {
+//     type: 'TICK',
+//     direction: Direction
+//   }
+
+// A GoUpAction is an object:
+//   { type: 'GO_UP' }
+
+// A GoRightAction is an object:
+//   { type: 'GO_RIGHT' }
+
+// A GoDownAction is an object:
+//   { type: 'GO_DOWN' }
+
+// A GoLeftAction is an object:
+//   { type: 'GO_LEFT' }
+
+// An ActionType is one of:
 //   - 'TICK'
 //   - 'GO_UP'
 //   - 'GO_RIGHT'
 //   - 'GO_DOWN'
 //   - 'GO_LEFT'
-const actions = {
+const actionTypes = {
   tick: 'TICK',
-  goUp: 'GO_UP',
-  goRight: 'GO_RIGHT',
-  goDown: 'GO_DOWN',
-  goLeft: 'GO_LEFT',
+  up: 'GO_UP',
+  right: 'GO_RIGHT',
+  down: 'GO_DOWN',
+  left: 'GO_LEFT',
+}
+
+// tick :: Direction -> TickAction
+// Given a direction, produces a tick action.
+const tick = direction => ({
+  type: actionTypes.tick,
+  direction,
+})
+
+// goUp :: -> GoUpAction
+const goUp = () => ({ type: actionTypes.up })
+
+// goRight :: -> GoRightAction
+const goRight = () => ({ type: actionTypes.right })
+
+// goDown :: -> GoDownAction
+const goDown = () => ({ type: actionTypes.down })
+
+// goLeft :: -> GoLeftAction
+const goLeft = () => ({ type: actionTypes.left })
+
+// -----------------------------------------------------------------------------
+// Functions
+// -----------------------------------------------------------------------------
+
+// nextState :: State * Action -> State
+// Given a state and an action, produces a next state.
+const nextState = (state = initialState, action) => {
+  if (!action) return state
+
+  return {
+    direction: nextDirection(state.direction, action),
+    snake: nextSnake(state.snake, action),
+  }
+}
+
+// nextDirection :: Direction * Action -> Direction
+// Given a state and an action, produces a next direction.
+const nextDirection = (direction, action) => {
+  switch (action.type) {
+    case actionTypes.up:
+      return directions.up
+    case actionTypes.right:
+      return directions.right
+    case actionTypes.down:
+      return directions.down
+    case actionTypes.left:
+      return directions.left
+    default:
+      return direction
+  }
+}
+
+// nextSnake :: Snake * Action -> Snake
+// Given a state and an action, produces a next snake.
+const nextSnake = (currentSnake, action) => {
+  switch (action.type) {
+    case actionTypes.tick: {
+      const newHead = nextHead(currentSnake[0], action.direction)
+      const tail = currentSnake.slice(0, currentSnake.length - 1)
+
+      return snake([ newHead, ...tail ])
+    }
+    default:
+      return currentSnake
+  }
+}
+
+const nextHead = (currentHead, direction) => {
+  let x = currentHead.x
+  let y = currentHead.y
+
+  if (direction === directions.up) {
+    y = y - 1
+  } else if (direction === directions.right) {
+    x = x + 1
+  } else if (direction === directions.down) {
+    y = y + 1
+  } else if (direction === directions.left) {
+    x = x - 1
+  }
+
+  return segment(x, y)
 }
 
 // -----------------------------------------------------------------------------
-// Render
+// View
 // -----------------------------------------------------------------------------
 
 class Game extends React.Component {
-  state = {
-    snake: [
-      { x: 3, y: 3 },
-    ],
-    direction: directions.right,
-  }
+  state = nextState()
 
   componentDidMount () {
-    setInterval(() => {
-      const newState = nextState(this.state, actions.tick)
-      this.setState(newState)
-    }, 500)
+    // Install keyboard input listener.
+    document.addEventListener('keyup', this.handleKey)
+
+    // Install tick interval timer.
+    setInterval(this.handleTick, 1000)
+  }
+
+  // handleTick :: -> void
+  handleTick = () => {
+    this.setState(state => nextState(state, tick(state.direction)))
+  }
+
+  // handleKey :: KeyboardEvent -> void
+  handleKey = ({ key }) => {
+    let action = null
+
+    if (key === 'ArrowUp' || key === 'k') {
+      action = goUp()
+    } else if (key === 'ArrowRight' || key === 'l') {
+      action = goRight()
+    } else if (key === 'ArrowDown' || key === 'j') {
+      action = goDown()
+    } else if (key === 'ArrowLeft' || key === 'h') {
+      action = goLeft()
+    }
+
+    if (action) {
+      this.setState(state => nextState(state, action))
+    }
   }
 
   render () {
     return (
-      <div
-        css={[ styles.container ]}
-      >
+      <div css={[ styles.container ]}>
         <Snake segments={this.state.snake} />
       </div>
     )
@@ -100,8 +240,8 @@ const Segment = ({ x, y }) => (
       position: 'absolute',
       left: `${x / SCENE_SIZE * 100}%`,
       top: `${y / SCENE_SIZE * 100}%`,
-      width: `${100 / SCENE_SIZE}%`,
-      height: `${100 / SCENE_SIZE}%`,
+      width: `${SEGMENT_SIZE}%`,
+      height: `${SEGMENT_SIZE}%`,
       borderRadius: '50%',
       backgroundColor: SNAKE_COLOR,
     }}
@@ -111,54 +251,12 @@ const Segment = ({ x, y }) => (
 const styles = {
   container: {
     position: 'relative',
+    overflow: 'hidden',
     width: '100%',
     height: 0,
     paddingBottom: '100%',
     backgroundColor: '#f1f3f5',
   },
-}
-
-// -----------------------------------------------------------------------------
-// Functions
-// -----------------------------------------------------------------------------
-
-// nextState :: State * Action -> State
-// Given a state and an action, produces a new state.
-const nextState = (state, action) => {
-  if (action === actions.tick) {
-    console.log(actions.tick)
-    const currentHead = state.snake[0]
-    let x = currentHead.x
-    let y = currentHead.y
-
-    if (state.direction === directions.up) {
-      console.log(1)
-      y = y - 1
-    } else if (state.direction === directions.right) {
-      console.log(2)
-      x = x + 1
-    } else if (state.direction === directions.down) {
-      console.log(3)
-      y = y + 1
-    } else if (state.direction === directions.left) {
-      console.log(4)
-      x = x - 1
-    }
-
-    const newSnake = snake([
-      segment(x, y),
-      ...state.snake.slice(0, state.snake.length - 1),
-    ])
-
-    console.log(newSnake)
-
-    return {
-      ...state,
-      newSnake,
-    }
-  }
-
-  return state
 }
 
 export default Game
