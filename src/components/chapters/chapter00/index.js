b9fd553080f8230ccd5b57136277824e80d778b5
Donghee Kim <kimdhoe1@gmail.com>
2019-01-16 22:38:48 +0900

    describe content briefly

--- src/components/chapters/chapter00/index.js
+++ src/components/chapters/chapter00/index.js
@@ -28,11 +28,39 @@ const Chapter00 = ({ title, title1 }) => (
       {' '}하는지 감을 잡지 못한 사람을 위한 튜토리얼입니다.
     </Paragraph>
 
-    <Snippet code={snippets.code01} />
+    {/*<Snippet code={snippets.code01} />*/}
 
     <Paragraph>
-      학교 종이 땡땡땡 어서 모이자. 선생님이 우리를 기다리신다.학교 종이 땡땡땡 어서 모이자. 선생님이 우리를 기다리신다.학교 종이 땡땡땡 어서 모이자. 선생님이 우리를 기다리신다.학교 종이 땡땡땡 어서 모이자. 선생님이 우리를 기다리신다.    학교 종이 땡땡땡 어서 모이자. 선생님이 우리를 기다리신다.
+      이 튜토리얼은 <InterestingWord text={'Web'} /> 기술을 다루고 있고 특히 화면을 그리기 위해서{' '}
+      <InterestingWord text={'React'} />에 의존하긴 하지만 특정 기술이나 라이브러리의
+      사용법을 알리려는 의도로 만들어진 것은 아닙니다. 세세한 부분에 너무 연연하기보단 작은 게임이나마
+      프로그램의 시작부터 완성까지, 어떤 사고와 과정을 거쳐 만들어지는지 알아가길 바랍니다.
     </Paragraph>
+
+    <Paragraph>
+      시작하기에 앞서 미리 귀뜀하자면, 우리는 아래의 질문들을 던지면서 진행해나가게 됩니다.
+    </Paragraph>
+
+    <ul>
+      <li css={{ fontStyle: 'italic' }}>
+        뱀 게임은 무엇인가?
+      </li>
+      <li css={{ fontStyle: 'italic' }}>
+        완성된 게임은 대략 어떤 모습인가?
+      </li>
+      <li css={{ fontStyle: 'italic' }}>
+        게임을 표현하는 데에 필요한 데이터는 어떤 것들이고, 무슨 의미를 갖는가?
+      </li>
+      <li css={{ fontStyle: 'italic' }}>
+        데이터를 어떻게 코드로 표현할 것인가?
+      </li>
+      <li css={{ fontStyle: 'italic' }}>
+        어떻게 화면에 출력할 것인가?
+      </li>
+      <li css={{ fontStyle: 'italic' }}>
+        게임 상에서 일어나는 변화를 어떻게 처리하고 반영할 것인가?
+      </li>
+    </ul>
   </Chapter>
 )
 
