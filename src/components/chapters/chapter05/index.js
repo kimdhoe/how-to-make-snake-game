7b67f475a75926ca923d2f9b01dc5fc7b64e199e
Donghee Kim <kimdhoe1@gmail.com>
2019-01-23 01:01:05 +0900

    add placeholder for chapter 5

---
+++ src/components/chapters/chapter05/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import {
+  Chapter,
+  Paragraph,
+  Important,
+  InterestingWord,
+  H3,
+  H4,
+  Code,
+  Snippet,
+  DataType,
+  Left,
+  Right,
+  StickyContainer,
+} from '../shared'
+import * as snippets from '../../../snippets'
+import ConstantsFigure from '../../constants-figure'
+import StateFigure from '../../state-figure'
+import EmptySceneFigure from '../../empty-scene-figure'
+import StillHeadFigure from '../../still-head-figure'
+import MovingHeadFigure from '../../moving-head-figure'
+
+const Chapter05 = ({ title, title1 }) => (
+  <Chapter number={5} title={'게임 오버 (Work in Progress)'}>
+    <Paragraph>
+      Coming soon.
+    </Paragraph>
+  </Chapter>
+)
+
+export default Chapter05
--- src/components/chapters/index.js
+++ src/components/chapters/index.js
@@ -5,6 +5,7 @@ import Chapter01 from './chapter01'
 import Chapter02 from './chapter02'
 import Chapter03 from './chapter03'
 import Chapter04 from './chapter04'
+import Chapter05 from './chapter05'
 
 const Chapters = ({ title, title1 }) => (
   <>
@@ -13,6 +14,7 @@ const Chapters = ({ title, title1 }) => (
     <Chapter02 title={title} title1={title1} />
     <Chapter03 title={title} title1={title1} />
     <Chapter04 title={title} title1={title1} />
+    <Chapter05 title={title} title1={title1} />
   </>
 )
 
